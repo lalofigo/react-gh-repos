22f3e1a24278076994c602b0104179a500a39c73
Gerardo Meza <gerardomeza@Gerardos-MacBook-Air.local>
2018-12-09 19:21:46 -0600

    Updates

---
+++ precache-manifest.8002d553d05a5250b7de92ca30b5b8ae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "820f924ab7953467dc31",
+    "url": "/react-gh-repos/static/css/main.ce1349f9.chunk.css"
+  },
+  {
+    "revision": "820f924ab7953467dc31",
+    "url": "/react-gh-repos/static/js/main.820f924a.chunk.js"
+  },
+  {
+    "revision": "796a18ab636c41d760cf",
+    "url": "/react-gh-repos/static/css/1.ce898c73.chunk.css"
+  },
+  {
+    "revision": "796a18ab636c41d760cf",
+    "url": "/react-gh-repos/static/js/1.796a18ab.chunk.js"
+  },
+  {
+    "revision": "33d88804b3ffc50728b0",
+    "url": "/react-gh-repos/static/js/runtime~main.33d88804.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/react-gh-repos/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/react-gh-repos/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/react-gh-repos/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/react-gh-repos/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/react-gh-repos/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e805facb18df82e91676a5944ef8cade",
+    "url": "/react-gh-repos/index.html"
+  }
+];
